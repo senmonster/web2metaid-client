b3ebd8168d3e80c0383919b071c05b3193033cda
jasonyang <yzsenmonster@gmail.com>
2024-07-12 15:25:06 +0800

    update serverURI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     TWITTER_CLIENT_ID: 'YlBZRXk4TGoxSE5NcmVURjhzeW06MTpjaQ',
-    SERVER_URI: 'http://www.localhost:3001',
+    SERVER_URI: 'http://8.134.124.195:3001',
   },
 };
