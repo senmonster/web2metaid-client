ff1e5955fa52f83f52333b925e493d77fabe64af
JasonYang <yzsenmonster@gmail.com>
2024-07-16 20:51:07 +0800

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,1 +1,25 @@
-module.exports = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/me',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
--- src/hooks/useTwitterInfoQuery.ts
+++ src/hooks/useTwitterInfoQuery.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
+axios.defaults.withCredentials = true;
 
 export type User = {
   id: string;
